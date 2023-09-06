158a440b3e1bdfc75414bcc23dfe1600328ca5be
zejr <zejunior.py@gmail.com>
2023-09-06 13:41:47 -0300

    Doc(): add http exceptions to doc -

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -3,8 +3,13 @@
 Item usado para disponibilizar as excessões personalizadas da aplicação.
 """
 
-from crud_error import CRUDCreateError, CRUDUpdateError, CRUDDeleteError, CRUDSelectError
-from http_error_schema import HTTPError
+from .crud_error import (  # isort:skip
+    CRUDCreateError,  # isort:skip
+    CRUDDeleteError,  # isort:skip
+    CRUDSelectError,  # isort:skip
+    CRUDUpdateError,  # isort:skip
+)  # isort:skip
+from .http_error_schema import HTTPError
 
 __all__ = [
     "CRUDCreateError",
